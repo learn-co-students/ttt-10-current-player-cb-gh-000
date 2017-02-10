d493c6c1012df57d8a72c76f0e145a3a5f617424
Bilal Ünal <mysoul444@gmail.com>
2017-02-10 14:56:43 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+  count = 0
+  board.each do |item|
+    if item == "X" || item == "O"
+      count += 1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  turn_count = turn_count(board)
+  if turn_count % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
