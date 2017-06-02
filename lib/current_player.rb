be2f58e152e2e3529c237d2bd77e5610d9ff7957
KajanM <kajan.14@cse.mrt.ac.lk>
2017-06-02 19:04:32 +0530

    count turn and decide which player's turn

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+def current_player(board)
+  count = turn_count(board)
+
+  if count % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+
+def turn_count(board)
+  count = 0
+  board.each do |item|
+    if item == "X" or item == "O"
+      count += 1
+    end
+  end
+  return count
+end
