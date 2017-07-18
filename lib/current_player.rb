9b120559424712c5092a97584ccdb8af6fa1f7bf
Hiram Torres <javier@scelabs.com>
2017-07-18 02:26:23 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  turn = turn_count(board)
+  turn.even? ? "X" : "O"
+end
