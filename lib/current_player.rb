1855d3102115d3c534525e5655a6b1db1ff65304
Rayana Sanzana <sanzanarayana@gmail.com>
2017-07-19 22:17:58 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,21 @@
+board = [" "," "," "," "," "," "," "," "," "]
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  elsif turn_count(board) % 2 != 0
+    return "O"
+  end
+
+end
+
+def turn_count(board)
+counter = 0
+
+board.each{|chars|
+  if chars == "X" || chars == "O"
+    counter += 1
+  end
+}
+return counter
+end
