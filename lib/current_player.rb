ffe9d1f4fa66b039eb0141e4821af95d422900cf
Muhammad Andriansyah <muh.andriansyah97@gmail.com>
2017-07-27 04:09:43 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+  turns = 0
+  board.each do | position |
+    if position != " "
+      turns +=1
+    end
+end
+  turns
+end
+
+def current_player(board)
+  turn = turn_count(board)
+  if turn % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
