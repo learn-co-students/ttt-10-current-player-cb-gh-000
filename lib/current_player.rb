5fdedb80084c33b52cd66bb762e038e04e9f8104
Oumar Ly <lyomrau@gmail.com>
2017-08-05 21:49:03 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+def turn_count(board)
+  count = 0
+  board.each do |block|
+    if block == "X" || block =="O"
+      count += 1
+    end
+  end
+  return count
+end
