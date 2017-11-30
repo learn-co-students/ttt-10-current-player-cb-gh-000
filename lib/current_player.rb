6ca1ce5e492b99faec86f13cafd80dcbd1a52e1f
Ahmad Taufiq Hidayat <ataufiq665@gmail.com>
2017-11-30 00:33:34 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,21 @@
+def turn_count(board)
+  counts=0;
+
+  board.each do |board|
+    if board == "X" || board == "O"
+      counts+=1
+    end
+  end
+
+  return counts
+end
+
+def current_player(board)
+
+  if turn_count(board)%2==0
+    return "X"
+  else
+    return "O"
+  end
+
+end
