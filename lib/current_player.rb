866151f4ac42649ab577c2b4732ff6ecbd56b47c
Youness A <158582@supinfo.com>
2018-04-27 21:33:09 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(board)
+  count = 0
+  board.each do |value|
+    if value == "X" || value == "O"
+      count += 1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  plays = turn_count(board)
+  return plays % 2 == 0 ? "X" : "O"
+end
