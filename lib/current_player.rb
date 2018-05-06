38026c39ed575a6186c5a1db1deea7346d7be168
Kyle Berkley <berkyle@gmail.com>
2018-05-06 08:46:53 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+ def turn_count(board)
+   count = 0
+   board.each do |index|
+     if index == "X" or index == "O"
+       count += 1
+     end
+   end
+   count
+ end
+
+ def current_player(board)
+   turns = turn_count(board)
+   if turns%2 == 0
+     return "X"
+   else
+     return "O"
+   end
+ end
