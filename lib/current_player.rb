81f018ce13a6b4c679639da072646108db571e96
sukhi kaur <sukhpreetchuhan@gmail.com>
2018-07-18 05:20:16 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,18 @@
-def TURN_COUNT(board)
+def turn_count(board)
+turns = 0
+ board.each do |array|
+   if array == "X" || array == "O"
+         turns += 1
+       end
+end
+ return turns
+ end
 
+def current_player(board)
+turns = turn_count(board)
+if turns.even?
+  return "X"
+else
+  return "O"
+end
 end
