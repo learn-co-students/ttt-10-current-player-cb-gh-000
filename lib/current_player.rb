4ca23da2a247d274913dcccaa1480cc3bb6dc7ca
Daniel Bremerkamp <daniel.bremerkamp@icloud.com>
2018-08-01 21:00:00 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,28 @@
+
+def turn_count(board)
+
+  turns = 0
+
+  board.each do | value |
+
+    if value == "X" || value == "O" then
+      turns += 1
+    end
+
+  end
+
+  turns
+
+end
+
+def current_player(board)
+
+  # Assumes X alway goes first
+
+  if turn_count(board) % 2 == 1 then
+    "O"
+  else
+    "X"
+  end
+
+end
