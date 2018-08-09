769f13239a95c5574c482b3066bb18df049c3d40
Shane Glass <shaneryanglass88@gmail.com>
2018-08-09 05:14:10 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,22 @@
+def turn_count(board)
+  count = 0
+  board.each do |turn|
+    if turn.include? "X" 
+      count += 1
+    elsif turn.include? "O"
+      count += 1
+    end
+  end
+  puts count
+  return count  
+end
+
+def current_player(board)
+  if turn_count(board) == 0 || turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+  
+  
