18b0a74f0343f637b451021bd6bbb9a602c72e72
Byambaa Bayarmandakh <byambaa0325@gmail.com>
2018-08-10 16:49:22 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,15 @@
+def turn_count(board)
+  count = 0
+  board.each do |cell|
+    if cell !=" "
+      count+=1
+    end
+  end
+  return count
+end
+def current_player(board)
+  if turn_count(board) % 2==0
+    return "X"
+  end
+  return "O"
+end
