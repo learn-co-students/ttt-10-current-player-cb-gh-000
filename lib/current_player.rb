13d9e0a3d741246529bc9cdd02c3971efbc40262
Andrew Turner <andrewturner227@gmail.com>
2018-11-20 17:13:14 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,15 @@
+def turn_count(board)
+    move_count = 0
+
+    board.each do |move|
+        if move == "X" || move == "O"
+            move_count += 1
+        end
+    end
+
+    move_count
+end
+
+def current_player(board)
+    turn_count(board).even? ? "X" : "O"
+end
